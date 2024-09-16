ddecd1bd1b454148c2908f98e94782852a3782f9
Uladzislau Kastsiukovich <145710560+sofaque@users.noreply.github.com>
2024-09-16 23:10:24 +0400

    Add files via upload

--- 2_data_governance/train.py
+++ 2_data_governance/train.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
-import matplotlib
+import json
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-X_train_res = pd.read_csv("X_train_res.csv")
-y_train_res = pd.read_csv("y_train_res.csv")
-X_test_scaled = pd.read_csv("X_test_scaled.csv")
-y_test = pd.read_csv("y_test_encoded.csv")
+X_train_res = pd.read_csv("X_train_res.csv", index_col=0)
+y_train_res = pd.read_csv("y_train_res.csv", index_col=0)
+X_test_scaled = pd.read_csv("X_test_scaled.csv", index_col=0)
+y_test = pd.read_csv("y_test_encoded.csv", index_col=0)
 
 # Logistic regression after implementing SMOTE
 lr = LogisticRegression()
