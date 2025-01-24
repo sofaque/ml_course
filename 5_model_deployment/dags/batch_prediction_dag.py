d8f2264b434cba451b9301b3d24288f9aa0fb19e
sofaque <sofapwnz@yandex.com>
2025-01-24 18:33:27 +0400

    changed dag to dockeroperator

--- 5_model_deployment/dags/batch_prediction_dag.py
+++ 5_model_deployment/dags/batch_prediction_dag.py
@@ -1,27 +1,28 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
-from datetime import datetime, timedelta
+from airflow.providers.docker.operators.docker import DockerOperator
+from datetime import datetime
 
 default_args = {
     "owner": "uladzislau",
     "depends_on_past": False,
     "email_on_failure": False,
-    "email_on_retry": False
-#    "retries": 1
+    "email_on_retry": False,
 }
 
 with DAG(
     "batch_prediction",
     default_args=default_args,
     description="Batch prediction DAG",
-    schedule_interval="0 * * * *",  # Каждые 60 минут
+    schedule_interval="0 * * * *",  # Every hour
     start_date=datetime(2025, 1, 1),
     catchup=False,
 ) as dag:
-    batch_task = BashOperator(
+    batch_task = DockerOperator(
         task_id="batch_predict",
-        bash_command=(
-            "python /opt/airflow/src/toxicity_detection/batch_predict.py"
- #           "/opt/airflow/data/input.csv /opt/airflow/data/output.csv"
-        )
+        image="5_model_deployment-api",  # Replace with the API image name
+        api_version="auto",
+        auto_remove=True,
+        docker_url="unix://var/run/docker.sock",  # Use the host's Docker
+        network_mode="bridge",
+        command="python src/toxicity_detection/batch_predict.py",
     )
